30a0bd65bbab2aef3428438ec140b90a2a702719
Owen Reaser <127154829+owreaser@users.noreply.github.com>
2024-05-15 13:08:50 -0400

    formatting

--- main.java
+++ main.java
@@ -1,7 +1,5 @@
-class Test
-{
-    public static void main(String []args)
-    {
-        System.out.println("My First Java Program.");
+class Test {
+    public static void main(String[] args) {
+        System.out.println("My First Java Program!");
     }
-};
+}
